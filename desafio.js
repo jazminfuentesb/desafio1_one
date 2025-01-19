7319b5e5a1c12872f55dbaf73f807900039fecf2
Jazmín Fuentes <140033691+jazminfuentesb@users.noreply.github.com>
2025-01-18 20:35:38 -0600

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,12 @@
+Desafíos
+
+Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+Declara una variable llamada nombre y asígnale el valor "Lua".
+Crea una variable llamada edady asígnale el valor 25.
+Establece una variable numeroDeVentas y asígnale el valor 50.
+Establece una variable saldoDisponible y asígnale el valor 1000.
+Muestra una alerta con el texto "¡Error! Completa todos los campos".
+Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre.
+Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección
